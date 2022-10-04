eaadce614fb74d7e51d8d0177084ea261c78ef1e
João Lourenção <jotap.lourencao@gmail.com>
2022-10-04 09:28:16 -0300

    chore: new interface in input component

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,14 +1,17 @@
 import { InputHTMLAttributes } from "react";
 
-interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface CustomInputProps {
   label?: string;
-  props?: InputHTMLAttributes<HTMLInputElement>
 }
 
-export function CustomInput({ label = "", ...props }: CustomInputProps) {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  data: CustomInputProps
+}
+
+export function CustomInput({ data, ...props }: Props) {
   return (
     <>
-      {label && (<label className='font-semibold text-base text-white' htmlFor={props.id}>{label}</label>)}
+      {data.label && (<label className='font-semibold text-base text-white' htmlFor={props.id}>{data.label}</label>)}
       <input
         className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
         {...props}
